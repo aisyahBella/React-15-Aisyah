fbff93ffd34040ac2b5871587742d76ce650dcd9
Rizky1408 <agungdwisatria23@gmail.com>
2022-03-26 15:43:28 +0800

    react testing

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,15 +1,13 @@
-import React from 'react';
+import React from "react";
 
 const TodoList = ({ todos }) => {
   return (
     <ul>
-      {
-        todos.map((todo) => (
-          <li>{todo.name}</li>
-        ))
-      }
+      {todos.map((todo) => (
+        <li>{todo.name}</li>
+      ))}
     </ul>
   );
-}
+};
 
 export default TodoList;
